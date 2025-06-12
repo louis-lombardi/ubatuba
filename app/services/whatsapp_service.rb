26e1e84a04414ba986610b88a483b52a437cb000
louis-lombardi <57060198+louis-lombardi@users.noreply.github.com>
2025-06-11 21:27:08 -0300

    Update whatsapp_service.rb

--- app/services/whatsapp_service.rb
+++ app/services/whatsapp_service.rb
@@ -9,6 +9,6 @@ class WhatsappService
 
   def call
     @client = Twilio::REST::Client.new(@sid,@token)
-    message = @client.messages.create(body: @text, from: "whatsapp:+15557631268",to: "whatsapp:#{@phone}")
+    message = @client.messages.create(body: @text, from: "whatsapp:+12727700781",to: "#{@phone}")
   end
 end
