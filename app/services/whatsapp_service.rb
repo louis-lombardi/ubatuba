f56d91e2c2fe66bc61158349cff55bb1cc1e47ca
louis-lombardi <57060198+louis-lombardi@users.noreply.github.com>
2025-06-15 21:13:40 -0300

    Update whatsapp_service.rb

--- app/services/whatsapp_service.rb
+++ app/services/whatsapp_service.rb
@@ -24,6 +24,18 @@ class WhatsappService
      call_template("HX6e3f39ca51b19d7ec820efa3c372547f")
   end
 
+  def call_new_chat_not_understood
+     call_template("HX8bdbe7315ad3fff47c73822ea5af70d4")
+  end
+
+  def call_new_chat_success
+     call_template("HX6e3f39ca51b19d7ec820efa3c372547f")
+  end
+
+  def call_new_chat_error
+     call_template("HX6e3f39ca51b19d7ec820efa3c372547f")
+  end
+
   def call_template(csid)
       @client = Twilio::REST::Client.new(@sid,@token)
       message = @client.messages.create(content_sid: csid, from: "whatsapp:+12727700781",to: "#{@phone}")
