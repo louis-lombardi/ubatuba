502c955e31923ebd92378a14b1d7ce7cd96c05fa
louis-lombardi <57060198+louis-lombardi@users.noreply.github.com>
2025-09-23 12:45:54 -0300

    Update 20250331093730_update_column_leads.rb

--- db/migrate/20250331093730_update_column_leads.rb
+++ db/migrate/20250331093730_update_column_leads.rb
@@ -1,7 +1,9 @@
+=begin
 class UpdateColumnLeads < ActiveRecord::Migration[5.2]
     def change
       change_column(:leads, :activities, :text)
       change_column(:leads, :additional_informations, :text)
     end
   end
+=end
   
