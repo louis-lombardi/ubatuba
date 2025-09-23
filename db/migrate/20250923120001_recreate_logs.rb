e7097aa9ac1e315f444155151ae3b64e7b5199f1
louis-lombardi <57060198+louis-lombardi@users.noreply.github.com>
2025-09-23 12:47:43 -0300

    Update 20250923120001_recreate_logs.rb

--- db/migrate/20250923120001_recreate_logs.rb
+++ db/migrate/20250923120001_recreate_logs.rb
@@ -1,6 +1,5 @@
 class RecreateLogs < ActiveRecord::Migration[5.2]
     def change
-      rename_table :logs, :logs_old
       create_table :logs do |t|
         t.string :source
         t.text :error
