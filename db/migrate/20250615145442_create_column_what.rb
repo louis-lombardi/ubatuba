cb00d5442184114ace8dca757a50d8719a66eb3b
louis-lombardi <57060198+louis-lombardi@users.noreply.github.com>
2025-09-23 12:47:30 -0300

    Update 20250615145442_create_column_what.rb

--- db/migrate/20250615145442_create_column_what.rb
+++ db/migrate/20250615145442_create_column_what.rb
@@ -1,5 +1,7 @@
-  class CreateColumnWhat < ActiveRecord::Migration[5.2]
+=begin
+class CreateColumnWhat < ActiveRecord::Migration[5.2]
     def change
       add_column :leads, :whats_number, :string
     end
   end
+=end
