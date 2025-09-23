56fb5e1939dd06edce12635964b9c4c0edadbae5
louis-lombardi <57060198+louis-lombardi@users.noreply.github.com>
2025-09-23 12:46:11 -0300

    Update 20250401232122_update_logs.rb

--- db/migrate/20250401232122_update_logs.rb
+++ db/migrate/20250401232122_update_logs.rb
@@ -1,7 +1,9 @@
+=begin
 class UpdateLogs < ActiveRecord::Migration[5.2]
     def change
       change_column(:logs, :error, :text)
       change_column(:leads, :backtrace, :text)
     end
 end
+=end
   
